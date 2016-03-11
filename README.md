63d326905ffcd20b7f8154a37cd69dee01a17aa6
syn <synwe@yahoo.fr>
2016-03-11 16:06:49 +0000

    Populate page management command

--- README.md
+++ README.md
@@ -137,7 +137,18 @@ Management commands
 --------------
 
 Create a homepage: `python manage.py create_homepage`
+
 Create a page: `python manage.py create_page 'Title' /url/`
+
+Populate pages with content:
+
+  ```python
+#~ from string:
+python manage.py populate_page "My content string"
+
+#~ from file
+python manage.py populate_page -f /my/file/path
+  ```
 
 
 Todo
---
+++ alapage/management/commands/populate_page.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+
+import os
+from django.core.management.base import BaseCommand, CommandError
+from alapage.models import Page
+from alapage.management.conf import bcolors
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def add_arguments(self, parser):
+        parser.add_argument('content', nargs='+', type=str)
+        parser.add_argument('-f',
+                            action='store_true',
+                            dest='file',
+                            default='',
+                            help='File to populate from',
+                            )
+    
+    def handle(self, *args, **options):
+        msg = 'What is the url of the page you want to populate?\n> '
+        url = raw_input(msg)
+        exists = Page.objects.filter(url=url).exists()
+        overwrite = False
+        #~ control page
+        if not exists:
+            print bcolors.WARNING +'!'+bcolors.ENDC+" Page "+url+" does not exist"
+            msg = "Do you want the page "+url+" to be created? [Y/n]\n> "
+            resp = raw_input(msg)
+            if resp =='' or resp=='Y':
+                msg2 = "Please type a title for the new page\n> "
+                title = raw_input(msg2)
+                page = Page.objects.create(url=url, title=title)
+            else:
+                print bcolors.FAIL +'X'+bcolors.ENDC+" Operation cancelled"
+                return
+        else:
+            page = Page.objects.get(url=url) 
+        if options['file']:
+            filepath = options['content'][0]
+            try:
+                #~ check if the file exists
+                if not os.path.isfile(filepath):
+                    print bcolors.FAIL +'X'+bcolors.ENDC+" File does not exist: operation cancelled"
+                    #print bcolors.FAIL +'X'+bcolors.ENDC+" Operation cancelled"
+                    return
+                else:
+                    #~ write the file
+                    with open(filepath, 'r') as content_file:
+                        content = content_file.read()
+            except Exception, e:
+                print str(e)
+        elif options['content']:
+            content = options['content'][0]
+        else:
+            print bcolors.FAIL +'X'+bcolors.ENDC+" You must provide arguments"
+        if len(page.content) > 0:
+            msg = 'The page already has content: do you really want to overwrite it? [y/N] \n> '
+            resp = raw_input(msg)
+            if resp == 'y':
+                overwrite = True
+            else:
+                print bcolors.FAIL +'X'+bcolors.ENDC+" Operation cancelled"
+                return
+        page.content = content
+        page.save()
+        if not overwrite:
+            print bcolors.OKGREEN+'Ok'+bcolors.ENDC+" Page "+url+" has been populated"
+        else:
+            print bcolors.OKGREEN+'Ok'+bcolors.ENDC+" Page "+url+" has been overwrited"
+        return
+
+
+
+
+
+
