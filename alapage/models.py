bc2ac5eac7d737481a3654400b7302c345ec8bd2
syn <synwe@yahoo.fr>
2016-03-17 17:39:44 +0000

    Admin templates

--- alapage/models.py
+++ alapage/models.py
@@ -3,6 +3,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.flatpages.models import FlatPage
+from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
 
 
