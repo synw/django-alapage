01cc82642273af950caedba471ed4cc9e788b615
Win <synwe@yahoo.com>
2017-08-11 14:12:18 +0000

    Fix url regex for no trailing slash

--- alapage/urls.py
+++ alapage/urls.py
@@ -1,15 +1,9 @@
-# -*- coding: utf-8 -*-
-
 from django.conf.urls import url
 from alapage.views import HomepageView, PageView, PagesmapView
 
 
-urlpatterns = []
-
-urlpatterns.append(
-    url(r'^sitemap/$', PagesmapView.as_view(), name="alapage-map"))
-#urlpatterns.append(url(r'^alapage/wizard/$', PageWizardView.as_view(), name="alapage-wizard"))
-#urlpatterns.append(url(r'^alapage/wizard/post/$', AddPagePostView.as_view(), name="alapage-wizard-post"))
-urlpatterns.append(
-    url(r'^(?P<url>.*?)$', PageView.as_view(), name="page-view"))
-urlpatterns.append(url(r'^', HomepageView.as_view(), name="home-view"))
+urlpatterns = [
+    url(r'^sitemap/?$', PagesmapView.as_view(), name="alapage-map"),
+    url(r'^(?P<url>.*?)?$', PageView.as_view(), name="page-view"),
+    url(r'^', HomepageView.as_view(), name="home-view"),
+]
