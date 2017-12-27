b24213551009179bf3081ddd9d1d283ae8863d55
Win <synwe@yahoo.com>
2017-12-27 10:45:19 +0000

    Corrections to make the tests work on travis

---
+++ travis_settings.py
@@ -0,0 +1,32 @@
+import os
+import django
+
+
+SECRET_KEY = "xyz"
+DEBUG = False
+TEMPLATE_DEBUG = True
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+}
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+    "ckeditor",
+    "ckeditor_uploader",
+    "codemirror2",
+    "mptt_graph",
+    "alapage",
+)
+
+django.setup()
