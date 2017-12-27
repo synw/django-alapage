3ab3592bf3b39b89ae1e292157c27deb96e86fd5
Win <synwe@yahoo.com>
2017-12-27 09:38:53 +0000

    Add a travis_settings file

---
+++ travis_settings.py
@@ -0,0 +1,10 @@
+SECRET_KEY = "xyz"
+DEBUG = False
+TEMPLATE_DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    },
+}
